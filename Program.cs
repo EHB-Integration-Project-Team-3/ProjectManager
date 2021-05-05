86712248eda20a9000789cc18cc817747f9ba40f
maaiked <67857316+maaiked@users.noreply.github.com>
2021-05-05 14:46:02 +0200

    helloWorld

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
